067876925885ee6d6f818d1b85be7e0e92ebece3
Nirmal Pol <33207203+nirmalpol@users.noreply.github.com>
2020-11-16 09:45:28 -0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,40 +1,36 @@
 from app import app
 from flask import render_template, url_for, flash, redirect, request
+from flask import jsonify
+from flask import send_file, send_from_directory
+import os
+import time
+from io import BytesIO
+import zipfile
+import shutil
+
+allFiles = []
 
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-
-    ''' 
-    TO DO ------------------------------------------------------------------
-    Write logic to retrieve user responses from recreational activity and 
-    customization option fields on html.
-
-    The only difficult part is to incorporate the javascript code I created to 
-    display the recreational activity images when user is hovering their mouse 
-    over the selection.
-    '''
-    #activity_resp1 = request.form.get('<insert assigned html <name> value>')
-    #cust_resp1 = request.form.get('<insert assigned html <name> value>')
-
-    '''             
-    TO DO ------------------------------------------------------------------
-    User will have pressed submit button before receiving file. Check if any
-    responses are missing or incorrectly filled out and present the messages
-    '''
-    
-    '''             
-    TO DO ------------------------------------------------------------------
-    Display preview of numpy-stl on the canvas of the html code so that the user 
-    can see what the file looks like on the website
-
-    Include downloadable link to the preview file
-    Include cost, time, image of the file and the user's selections
-    '''
-
-
     return render_template('main.html')
 
+''' Acquire file paths from javascript code for the user's selected files.
+    Store the names of the paths of these files. The /return_files function
+    will return the actual file paths once the user clicks the download button.'''
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    global allFiles
+    allFiles = []
+    data = request.get_json()
+    for i in range(len(data)):
+        filename = data[i]
+        filename = filename.replace('../', '')
+        filename = filename.replace('/', '\\')
+        
+        allFiles.append(filename[0:-9])
+    print(allFiles)
+    return jsonify(status="success", data=data)
 
 
 def database(parameters):
@@ -68,6 +64,18 @@ def database(parameters):
         even further and maybe reconsider this approach later on.
         
     '''
+@app.route('/return-files/')
+def return_files():
+    try:
+        global allFiles
+        file_path = os.getcwd() + '\\app\\' + allFiles[0]
+    
+        return send_file(shutil.make_archive('myFiles', 'zip', file_path),
+                        attachment_filename='myFiles.zip',
+                        as_attachment=True)
+    except Exception as e:
+        print(os.getcwd())
+        return str(e)
 
 @app.route('/About')
 def toAboutPage():
@@ -80,4 +88,3 @@ def toContactPage():
 @app.route('/Disclaimer')
 def toDisclaimerPage():
     return render_template('Disclaimer.html')
-    
