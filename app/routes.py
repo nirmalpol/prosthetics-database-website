5ebc452ea4a4a05fefd788fc41e951ab7d35ad1e
nirmalpol <nirmalpol2012@hotmail.com>
2020-11-17 13:16:00 -0500

    updated routes for download button

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,7 @@ from io import BytesIO
 import zipfile
 import shutil
 
-allFiles = []
+allFiles = None
 
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
@@ -21,61 +21,29 @@ def index():
 @app.route('/handle_data', methods=['POST'])
 def handle_data():
     global allFiles
-    allFiles = []
     data = request.get_json()
     for i in range(len(data)):
         filename = data[i]
         filename = filename.replace('../', '')
         filename = filename.replace('/', '\\')
         
-        allFiles.append(filename[0:-9])
+        allFiles = filename[0:-9]
     print(allFiles)
     return jsonify(status="success", data=data)
 
 
-def database(parameters):
-    '''
-        Set parameters as a list object.
-        Determine logic for deciding how to return user desired prosthetic file.
-
-        
-        Storage file directory
-            - Recreational Activity 1
-                - Left Hand
-                    - File 1 L_B
-                    - File 1 L_M
-                    - File 1 L_S
-                - Right Hand
-                    - File 1 L_B
-                    - File 1 L_M
-                    - File 1 L_S
-
-            - Recreational Activity 2
-            - Recreational Activity 3
-
-        EXAMPLE:
-        Based on the parameters received, you will have identified the recreational activity,
-        the handedness, and the size. Simply based on these three parameters, we could just
-        have the function search the path and return:
-
-        "/Recreational Activity 1/Left Hand/ File 1 L_B"
-        
-        We might not need to even have a SQL database. This could simplify the problem
-        even further and maybe reconsider this approach later on.
-        
-    '''
-@app.route('/return-files/')
+@app.route('/return_files/', methods=['POST'])
 def return_files():
-    try:
-        global allFiles
-        file_path = os.getcwd() + '\\app\\' + allFiles[0]
-    
-        return send_file(shutil.make_archive('myFiles', 'zip', file_path),
-                        attachment_filename='myFiles.zip',
-                        as_attachment=True)
-    except Exception as e:
-        print(os.getcwd())
-        return str(e)
+
+    global allFiles
+    file_path = os.getcwd() + '\\app\\' + allFiles
+    print(os.getcwd())
+    print(file_path)
+    return send_file(shutil.make_archive('myFiles', 'zip', file_path),
+                    attachment_filename='myFiles.zip',
+                    as_attachment=True)
+
+
 
 @app.route('/About')
 def toAboutPage():
